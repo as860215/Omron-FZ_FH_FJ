5a9fcecf48f8b7edb02ce4fcf4460d33b5f3ffd7
as860215 <as860215@yahoo.com.tw>
2018-11-26 09:13:56 +0800

    Omron FZ_FH_FJ
    Connect your Omron camera with Ethernet.

---
+++ OmronFZ_FH_FJ/OmronFZ_FH_FJ/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace OmronFZ_FH_FJ_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_ip = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.lb_status = new System.Windows.Forms.Label();
+            this.btn_photo = new System.Windows.Forms.Button();
+            this.tb_get = new System.Windows.Forms.TextBox();
+            this.timer_get = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Address";
+            // 
+            // tb_ip
+            // 
+            this.tb_ip.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ip.Location = new System.Drawing.Point(16, 31);
+            this.tb_ip.Name = "tb_ip";
+            this.tb_ip.Size = new System.Drawing.Size(100, 26);
+            this.tb_ip.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(151, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // tb_port
+            // 
+            this.tb_port.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_port.Location = new System.Drawing.Point(154, 31);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.Size = new System.Drawing.Size(100, 26);
+            this.tb_port.TabIndex = 3;
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_connect.Location = new System.Drawing.Point(271, 13);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(108, 43);
+            this.btn_connect.TabIndex = 4;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // lb_status
+            // 
+            this.lb_status.AutoSize = true;
+            this.lb_status.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_status.Location = new System.Drawing.Point(269, 74);
+            this.lb_status.Name = "lb_status";
+            this.lb_status.Size = new System.Drawing.Size(20, 18);
+            this.lb_status.TabIndex = 5;
+            this.lb_status.Text = "...";
+            // 
+            // btn_photo
+            // 
+            this.btn_photo.Enabled = false;
+            this.btn_photo.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_photo.Location = new System.Drawing.Point(271, 177);
+            this.btn_photo.Name = "btn_photo";
+            this.btn_photo.Size = new System.Drawing.Size(108, 43);
+            this.btn_photo.TabIndex = 7;
+            this.btn_photo.Text = "Measure";
+            this.btn_photo.UseVisualStyleBackColor = true;
+            this.btn_photo.Click += new System.EventHandler(this.btn_photo_Click);
+            // 
+            // tb_get
+            // 
+            this.tb_get.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_get.Location = new System.Drawing.Point(16, 74);
+            this.tb_get.Multiline = true;
+            this.tb_get.Name = "tb_get";
+            this.tb_get.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_get.Size = new System.Drawing.Size(238, 146);
+            this.tb_get.TabIndex = 8;
+            // 
+            // timer_get
+            // 
+            this.timer_get.Interval = 1;
+            this.timer_get.Tick += new System.EventHandler(this.timer_get_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 232);
+            this.Controls.Add(this.tb_get);
+            this.Controls.Add(this.btn_photo);
+            this.Controls.Add(this.lb_status);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.tb_port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_ip);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Omron FZ_FH_FJ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_ip;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Label lb_status;
+        private System.Windows.Forms.Button btn_photo;
+        private System.Windows.Forms.TextBox tb_get;
+        private System.Windows.Forms.Timer timer_get;
+    }
+}
+
---
+++ OmronFZ_FH_FJ/OmronFZ_FH_FJ/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace OmronFZ_FH_FJ_{
+    public partial class Form1 : Form{
+        Socket Sck; // 先行宣告Socket
+        string SckIp = "10.5.6.100";
+        string SckRec = "";
+        List<string> tem_Rec = new List<string>();
+        int show_count = 0;
+        int SckPort = 9876;
+        int RDataLen = 15;  // 以長度為15傳送資料
+        public Form1(){
+            InitializeComponent();
+            tb_ip.Text = SckIp;
+            tb_port.Text = SckPort.ToString();
+        }
+        private void btn_connect_Click(object sender, EventArgs e){
+            if(btn_connect.Text == "Connect"){
+                lb_status.Text = "Start connect...";
+                if(PingIP()) ConnectServer();
+                else{
+                    lb_status.Text = "IP error.\nPlease check \nand try again.";
+                    return;
+                }
+                if (Sck.Connected == true){
+                    lb_status.Text = "Connecting \nsuccess.";
+                    btn_photo.Enabled = true;
+                    btn_connect.Text = "Disconnect";
+                }
+                else lb_status.Text = "Connecting \nfailed.";
+            }
+            else{
+                btn_photo.Enabled = false;
+                Sck.Close();
+                btn_connect.Text = "Connect";
+            }
+        }
+        /// <summary>
+        /// 利用IPAddress屬性配合Ping進行遠端Server的確認。
+        /// </summary>
+        /// <returns></returns>
+        public bool PingIP(){
+            IPAddress tIP = IPAddress.Parse(tb_ip.Text);
+            Ping tPingControl = new Ping();
+            PingReply tReply = tPingControl.Send(tIP);
+            tPingControl.Dispose();
+            if (tReply.Status != IPStatus.Success)
+                return false;
+            else
+                return true;
+        }
+    // 連線
+    private void ConnectServer(){
+            try{
+                Sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Sck.SendTimeout = 2000;
+                Sck.Connect(new IPEndPoint(IPAddress.Parse(tb_ip.Text), int.Parse(tb_port.Text)));
+                // RmIp和SPort分別為string和int型態, 前者為Server端的IP, 後者為Server端的Port
+                // 同 Server 端一樣要另外開一個執行緒用來等待接收來自 Server 端傳來的資料, 與Server概念同
+                Sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 0);
+                Sck.ReceiveTimeout = 0;
+                Thread SckSReceiveTd = new Thread(SckSReceiveProc);
+                SckSReceiveTd.Start();
+            }
+            catch { }
+        }
+        private void SckSReceiveProc(){
+            try{
+                long IntAcceptData;
+                byte[] clientData = new byte[RDataLen];
+                while (true){
+                    // 程式會被 hand 在此, 等待接收來自 Server 端傳來的資料
+                    IntAcceptData = Sck.Receive(clientData);
+                    // 接收到來自Server端的資料”
+                    SckRec = Encoding.Default.GetString(clientData);
+                    string[] tem_split = SckRec.Split('\r');
+                    tem_split = tem_split[0].Split(',');
+                    tem_split = tem_split[0].Split(' ');
+                    foreach (string s in tem_split){
+                        if (s != ""){
+                            SckRec = s;
+                            break;
+                        }
+                    }
+                    tem_Rec.Add(SckRec);
+                    show_count++;
+                    //接收Socketc回傳訊息後執行指令
+                    clientData = new byte[RDataLen];
+                }
+            }
+            catch(Exception ex) {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+        private void SckSSend(string send){
+            try{
+                Sck.Send(Encoding.ASCII.GetBytes(send));
+            }
+            catch { }
+        }
+        private void btn_photo_Click(object sender, EventArgs e){
+            tem_Rec.Clear();
+            tb_get.Text = "";
+            timer_get.Enabled = true;
+            SckSSend("m");
+        }
+        private void timer_get_Tick(object sender, EventArgs e){
+            if (tem_Rec.Count > 0){
+                foreach (string s in tem_Rec) tb_get.Text += s + "\r\n";
+                tem_Rec.Clear();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e){
+            if(Sck!=null) Sck.Close();
+        }
+    }
+}
